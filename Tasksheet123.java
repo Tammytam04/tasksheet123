a7f8bc80f4dd08ac646ffd38d4dc9e33da507993
John Paul Ebid <120429052+Tammytam04@users.noreply.github.com>
2024-09-06 17:43:51 +0800

    Create Tasksheet123.java

---
+++ Tasksheet123.java
@@ -0,0 +1,40 @@
+class Book {
+    private String title;
+    private String author;
+    private int yearPublished;
+    private double price;
+
+    public Book(String title, String author, int yearPublished, double price) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.price = price;
+    }
+
+    public void printDetails() {
+        System.out.println("Title: \"" + title + "\"");
+        System.out.println("Author: \"" + author + "\"");
+        System.out.println("Year Published: " + yearPublished);
+        System.out.println("Price: $" + price);
+    }
+}
+public class Tasksheet123 {
+    public static void main(String[] args) {
+        
+        Book book1 = new Book("Java Programming", "John Smith", 2021, 39.99);
+        Book book2 = new Book("Python Basics", "Jane Doe", 2020, 29.99);
+        Book book3 = new Book("C++ Essentials", "Michael Johnson", 2019, 49.99);
+
+        System.out.println("Book 1:");
+        book1.printDetails();
+        System.out.println();
+        
+        System.out.println("Book 2:");
+        book2.printDetails();
+        System.out.println();
+        
+        System.out.println("Book 3:");
+        book3.printDetails();
+        System.out.println();
+    }
+}
